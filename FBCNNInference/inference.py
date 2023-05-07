3c7149094f3b8f9217e43e0fbd37f2c5f85b319d
yuan <ayrw@163.com>
2023-05-07 20:13:08 +0800

    AddFBCNN  and Yolov7

---
+++ FBCNNInference/inference.py
@@ -0,0 +1,41 @@
+import torch
+import torchvision
+from PIL import Image
+import os
+from .models.network_fbcnn import FBCNN as net
+import numpy as np
+
+class Predictor():
+    def __init__(self, weightsDir='.') -> None:
+        n_channels = 3            # set 1 for grayscale image, set 3 for color image
+        model_path = os.path.join(weightsDir, 'fbcnn_color.pth')
+        nc = [64, 128, 256, 512]
+        nb = 4
+        self.model = net(in_nc=n_channels, out_nc=n_channels,
+                         nc=nc, nb=nb, act_mode='R')
+        self.model.load_state_dict(torch.load(model_path), strict=True)
+        self.model.to('cuda')
+        self.model.eval()
+
+    def predict(self, img):
+
+        img_L = torch.from_numpy(np.ascontiguousarray(img)).permute(2, 0, 1).float().div(255.).unsqueeze(0)
+        img_L = img_L.to('cuda')
+
+        # ------------------------------------
+        # (2) img_E
+        # ------------------------------------
+
+        #img_E,QF = model(img_L, torch.tensor([[0.6]]))
+        QF = self.model(img_L)
+        QF = 1 - QF
+        QF = round(float(QF*100))
+        # logger.info('predicted quality factor: {:d}'.format(round(float(QF*100))))
+        # util.imsave(img_E, os.path.join(E_path, img_name+'.png'))
+        return {'QF': QF}
+
+
+def pil_loader(path):
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
---
+++ Yolov7Inference/inference_yolov7.py
@@ -0,0 +1,136 @@
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+
+def detect(save_img=False):
+    source, weights, view_img, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.img_size, not opt.no_trace
+
+    # Initialize
+    device = select_device(opt.device)
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
+    if trace:
+        model = TracedModel(model, device, opt.img_size)
+
+    if half:
+        model.half()  # to FP16
+
+    # Set Dataloader
+    dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
+
+    t0 = time.time()
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Warmup
+        # if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+        #     old_img_b = img.shape[0]
+        #     old_img_h = img.shape[2]
+        #     old_img_w = img.shape[3]
+        #     for i in range(3):
+        #         model(img, augment=opt.augment)[0]
+
+        # Inference
+        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+            pred = model(img, augment=opt.augment)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+
+            fixed_height = 320
+            height_percent = (fixed_height / float(im0.shape[1]))
+            width_size = int((float(im0.shape[0]) * float(height_percent)))
+            
+            im0 = cv2.resize(im0,(fixed_height,width_size))
+            p = Path(p)  # to Path
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    label = f'{names[int(cls)]} {conf:.2f}'
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+
+
+            # Stream results
+            if view_img:
+                cv2.imshow(str('test'), im0)
+                cv2.waitKey(0)  # 1 millisecond
+
+
+
+    print(f'Done. ({time.time() - t0:.3f}s)')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.5, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+    opt = parser.parse_args()
+    opt.weights = r'F:\NSFW_DS\ImageInfoExtractor\DLToolWeights\yolov7-e6e.pt'
+    opt.source = r"G:\照片媒体\SD卡原始照片\REDMI_NOTE5_PRO\Camera"
+    opt.view_img = True
+    opt.no_trace = True
+    print(opt)
+
+    with torch.no_grad():
+        if opt.update:  # update all models (to fix SourceChangeWarning)
+            for opt.weights in ['yolov7.pt']:
+                detect()
+                strip_optimizer(opt.weights)
+        else:
+            detect()
