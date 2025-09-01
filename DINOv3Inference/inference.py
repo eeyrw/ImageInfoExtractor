af583a0b96f299534e970d76e44315300c21a34f
Your Name <you@example.com>
2025-09-01 20:50:25 +0800

    M

--- DINOv3Inference/inference.py
+++ DINOv3Inference/inference.py
@@ -3,34 +3,38 @@ from __future__ import annotations
 import PIL.Image
 import torch
 from tqdm import tqdm
-from transformers import DINOv3ViTImageProcessorFast,DINOv3ViTModel
+from transformers import DINOv3ViTImageProcessorFast, DINOv3ViTModel
 from pillow_heif import register_heif_opener
 register_heif_opener()
-
-
 
 
 class Predictor():
     def __init__(self, weightsDir='.', device='cuda:6') -> None:
         self.device = device
         pretrained_model_name = "facebook/dinov3-vitl16-pretrain-lvd1689m"
-        self.processor = DINOv3ViTImageProcessorFast.from_pretrained(pretrained_model_name)
+        self.processor = DINOv3ViTImageProcessorFast.from_pretrained(
+            pretrained_model_name,
+            dtype=torch.bfloat16
+            )
         self.model = DINOv3ViTModel.from_pretrained(
-            pretrained_model_name, 
-            device_map=self.device, 
+            pretrained_model_name,
+            device_map=self.device,
             local_files_only=False,
-            cache_dir=weightsDir
+            cache_dir=weightsDir,
+            dtype=torch.bfloat16
         )
         self.model.eval()
 
     def predict(self, raw_image):
 
         # Data settings
-        inputs = self.processor(images=raw_image, return_tensors="pt").to(self.model.device)
+        inputs = self.processor(images=raw_image, return_tensors="pt", size={
+                                'width': 512, 'height': 512}).to(self.model.device)
         with torch.inference_mode():
             outputs = self.model(**inputs)
 
-        return {"IMG_EMBD":outputs.pooler_output.squeeze().cpu().numpy().tolist()}
+        return {"IMG_EMBD": outputs.pooler_output.squeeze().
+                to(device='cpu', dtype=torch.float32).numpy().tolist()}
 
 
 if __name__ == '__main__':
